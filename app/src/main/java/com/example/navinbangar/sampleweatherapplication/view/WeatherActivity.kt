14c55bb1c1c37fc81d4d6af4509c70eb2329578b
navinkumarbangar <naveen.bangar27@gmail.com>
2019-11-19 18:09:48 +0530

    moved activity class in view package

--- app/src/main/java/com/example/navinbangar/sampleweatherapplication/view/WeatherActivity.kt
+++ app/src/main/java/com/example/navinbangar/sampleweatherapplication/view/WeatherActivity.kt
@@ -1,7 +1,8 @@
-package com.example.navinbangar.sampleweatherapplication
+package com.example.navinbangar.sampleweatherapplication.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navinbangar.sampleweatherapplication.R
 
 class WeatherActivity : AppCompatActivity() {
 
