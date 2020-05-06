12fccc2f0ee2ab31327ca564d8ec0a1fcd2a33d2
navinkumarbangar <naveen.bangar27@gmail.com>
2020-05-06 20:12:24 +0530

    added apksign task in azure-pipeline.yml file

--- app/src/androidTest/java/com/example/navinbangar/sampleweatherapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/navinbangar/sampleweatherapplication/ExampleInstrumentedTest.kt
@@ -23,7 +23,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.navinbangar.sampleweatherapplication.free.debug", appContext.packageName)
+        assertEquals("com.example.navinbangar.sampleweatherapplication", appContext.packageName)
     }
 
     @After
